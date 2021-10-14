fd6960ec8ebab83df3f068673423908431694619
Samuel Quinzada <sqes26@gmail.com>
2021-10-14 00:31:13 -0500

    Análisis Salarial

---
+++ salarios.js
@@ -0,0 +1,81 @@
+const panama = [];
+panama.push({
+  name: "Camila",
+  salary: 500,
+});
+panama.push({
+  name: "Nath",
+  salary: 1500,
+});
+panama.push({
+  name: "Luisa",
+  salary: 1800,
+});
+panama.push({
+  name: "Laura",
+  salary: 1000,
+});
+panama.push({
+  name: "Daniela",
+  salary: 2200,
+});
+panama.push({
+  name: "Esperancita",
+  salary: 200,
+});
+panama.push({
+  name: "Carla",
+  salary: 500,
+});
+panama.push({
+  name: "Antonieta",
+  salary: 1500,
+});
+panama.push({
+  name: "Alicia",
+  salary: 1300,
+});
+panama.push({
+  name: "Ana",
+  salary: 2400,
+});
+panama.push({
+  name: "Julia",
+  salary: 3400,
+});
+panama.push({
+  name: "Rosa",
+  salary: 400,
+});
+panama.push({
+  name: "Angélica",
+  salary: 400,
+});
+panama.push({
+  name: "Tatiana",
+  salary: 400,
+});
+panama.push({
+  name: "Lorena",
+  salary: 600,
+});
+panama.push({
+  name: "Carolina",
+  salary: 1600,
+});
+panama.push({
+  name: "Fernanda",
+  salary: 2600,
+});
+panama.push({
+  name: "Nora",
+  salary: 1000,
+});
+panama.push({
+  name: "Gisselle",
+  salary: 2000,
+});
+panama.push({
+  name: "Bill Gates",
+  salary: 100000000,
+});
